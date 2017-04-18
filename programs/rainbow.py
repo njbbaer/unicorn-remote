bb0a2d3affaa9caa2791eeede41639559c037904
Nathaniel Baer <njbbaer@gmail.com>
2017-04-18 02:11:54 -0700

    Run python script when program is clicked & refactor

---
+++ programs/rainbow.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import math
+import time
+
+import unicornhat as unicorn
+
+unicorn.set_layout(unicorn.AUTO)
+unicorn.rotation(0)
+width,height=unicorn.get_shape()
+
+i = 0.0
+offset = 30
+while True:
+        i = i + 0.3
+        for y in range(height):
+                for x in range(width):
+                        r = 0
+                        g = 0
+                        r = (math.cos((x+i)/2.0) + math.cos((y+i)/2.0)) * 64.0 + 128.0
+                        g = (math.sin((x+i)/1.5) + math.sin((y+i)/2.0)) * 64.0 + 128.0
+                        b = (math.sin((x+i)/2.0) + math.cos((y+i)/1.5)) * 64.0 + 128.0
+                        r = max(0, min(255, r + offset))
+                        g = max(0, min(255, g + offset))
+                        b = max(0, min(255, b + offset))
+                        unicorn.set_pixel(x,y,int(r),int(g),int(b))
+        unicorn.show()
+        time.sleep(0.01)
--- remote.py
+++ remote.py
@@ -3,35 +3,52 @@ from flask_restful import Resource, Api
 import unicornhat as unicorn
 import argparse
 import json
+import subprocess
+import sys
 
 app = Flask(__name__)
 
-state = {
-    'brightness': 50,
-    'program': None,
-}
+class State:
+    ''' Handle Unicorn HAT state'''
 
-def set_brightness(brightness):
-    state['brightness'] = brightness
-    unicorn.brightness(brightness/100)
+    def __init__(self, brightness, program_name):
+        self.process = None
+        self.set_brightness(brightness)
+        self.set_program(program_name)
 
-def set_program(program_name):
-    state['program'] = program_name
+    def set_brightness(self, brightness):
+        self.brightness = brightness
+        unicorn.brightness(self.brightness/100)
+
+    def set_program(self, program_name):
+        self.program_name = program_name
+
+        # Terminate running program
+        if self.process is not None:
+            self.process.terminate()
+
+        # Start new program
+        if self.program_name is not None:
+            path = 'programs/' + self.program_name + '.py'
+            self.process = subprocess.Popen([sys.executable, path])
+
+# Initialize unicorn state
+state = State(brightness=50, program_name=None)
 
 @app.route('/', methods=['POST', 'GET'])
-def home(brightness=state['brightness'], 
-         program=state['program']):
+def home(brightness=state.brightness, 
+         program=state.program_name):
 
     if request.method == 'POST':
 
         if 'brightness' in request.form:
-            set_brightness(int(request.form['brightness']))
+            state.set_brightness(int(request.form['brightness']))
 
         if 'program' in request.form:
-            set_program(request.form['program'])
+            state.set_program(request.form['program'])
             
-    return render_template('home.html', brightness=state['brightness'],
-                                        program=state['program'])
+    return render_template('home.html', brightness=state.brightness,
+                                        program_name=state.program_name)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
