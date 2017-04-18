d1ccd053fc435c8ae857a001048e1564b6090238
Nathaniel Baer <njbbaer@gmail.com>
2017-04-18 01:00:06 -0700

    Create global state & refactor

--- remote.py
+++ remote.py
@@ -1,21 +1,37 @@
-from flask import Flask, render_template, request, g
-import argparse
+from flask import Flask, render_template, request
+from flask_restful import Resource, Api
 import unicornhat as unicorn
+import argparse
 import json
 
 app = Flask(__name__)
 
+state = {
+    'brightness': 50,
+    'program': None,
+}
+
+def set_brightness(brightness):
+    state['brightness'] = brightness
+    unicorn.brightness(brightness/100)
+
+def set_program(program_name):
+    state['program'] = program_name
 
 @app.route('/', methods=['POST', 'GET'])
-def home(brightness=50, program=None):
-    if request.method == 'POST':
-        brightness = int(request.form['brightness'])
-        program = request.form['program']
-        unicorn.brightness(brightness/100)
-        print('running program', program, 'at', brightness)
-            
-    return render_template('home.html', brightness=brightness, program=program)
+def home(brightness=state['brightness'], 
+         program=state['program']):
 
+    if request.method == 'POST':
+
+        if 'brightness' in request.form:
+            set_brightness(int(request.form['brightness']))
+
+        if 'program' in request.form:
+            set_program(request.form['program'])
+            
+    return render_template('home.html', brightness=state['brightness'],
+                                        program=state['program'])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
