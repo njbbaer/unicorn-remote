85f45f3ee1446d0a337c8e8052406acb012fe564
Nathaniel Baer <njbbaer@gmail.com>
2017-07-31 23:01:40 -0700

    Add dna program & remove grid_pic

--- app/programs/dna.py
+++ app/programs/dna.py
@@ -8,22 +8,20 @@ def run(params):
     # Every line needs to be exactly 8 characters
     # but you can have as many lines as you like.
     ASCIIPIC = [
-         "  X  X  "
-        ,"  X  X  "
-        ,"        "
-        ," X    X "
-        ,"  XXXX  "
-        ,"        "
-        ,"        "
-        ,"        "
-        ,"  X  X  "
-        ,"  X  X  "
-        ,"        "
-        ,"  XXXX  "
-        ," X    X "
-        ,"        "
-        ,"        "
-        ,"        "
+         "1     2 "
+        ," 1     2"
+        ,"  1   2 "
+        ,"   1 2  "
+        ,"    1   "
+        ,"   2 1  "
+        ,"  2   1 "
+        ," 2     1"
+        ,"2     1 "
+        ," 2   1  "
+        ,"  2 1   "
+        ,"   2    "
+        ,"  1 2   "
+        ," 1   2  "
         ]
     global i
     i = -1
@@ -35,12 +33,14 @@ def run(params):
             for w in range(width):
                 hPos = (i+h) % len(ASCIIPIC)
                 chr = ASCIIPIC[hPos][w]
-                if chr == ' ':
-                    unicorn.set_pixel(w, h, 0, 0, 0)
+                if chr == '1':
+                    unicorn.set_pixel(w, h, 255, 255, 255)
+                elif chr == '2':
+                    unicorn.set_pixel(w, h, 0, 0, 255)
                 else:
-                    unicorn.set_pixel(w, h, 255, 0, 0)
+                    unicorn.set_pixel(w, h, 0, 0, 0)
         unicorn.show()
 
     while True:
         step()
-        sleep(0.2)
+        sleep(0.1)
