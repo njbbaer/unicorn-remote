705cd4b1febb486b9a84e59a6c257396d58f5aa7
Nathaniel Baer <njbbaer@gmail.com>
2017-08-06 18:50:56 -0700

    Refactor api error handling

--- app/api.py
+++ app/api.py
@@ -9,13 +9,7 @@ class PostProgram(Resource):
     def post(self, program_name):
             args = request.args
 
-            if 'brigthness' in args:
-                if not 0 <= float(args['brightness']) <= 1:
-                    abort(404, error="Brigthness must be a float between 0 and 1")
-
-            if 'rotation' in args:
-                print(args['rotation'])
-                if not int(args['rotation']) in [0, 90, 180, 270]:
-                    abort(404, error="Rotation must be 0, 90, 180, or 270 degrees")
-
-            state.start_program(program_name, args)
+            try:
+                state.start_program(program_name, args)
+            except ValueError as e:
+                abort(400, error=str(e))
