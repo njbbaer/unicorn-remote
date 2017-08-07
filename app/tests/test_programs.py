72b1521f5e5501be191e203aa7df20dd27ea0ed4
Nathaniel Baer <njbbaer@gmail.com>
2017-08-06 18:04:17 -0700

    Reorganize tests into package

--- app/tests/test_programs.py
+++ app/tests/test_programs.py
@@ -12,67 +12,67 @@ class TestPrograms(unittest.TestCase):
         self.process.terminate()
 
     def test_program_ascii_text(self):
-        from programs.ascii_text import run
+        from app.programs.ascii_text import run
         self.program_helper(run, { "text": "Hello World" }) 
 
     def test_cheertree(self):
-        from programs.cheertree import run
+        from app.programs.cheertree import run
         self.program_helper(run)
 
     def test_crss(self):
-        from programs.cross import run
+        from app.programs.cross import run
         self.program_helper(run)
 
     def test_demo(self):
-        from programs.demo import run
+        from app.programs.demo import run
         self.program_helper(run)
 
     def test_dna(self):
-        from programs.dna import run
+        from app.programs.dna import run
         self.program_helper(run)
 
     def test_game_of_life(self):
-        from programs.game_of_life import run
+        from app.programs.game_of_life import run
         self.program_helper(run)
 
     def test_dna(self):
-        from programs.dna import run
+        from app.programs.dna import run
         self.program_helper(run)
 
     def test_program_matrix(self):
-        from programs.matrix import run
+        from app.programs.matrix import run
         self.program_helper(run)
 
     def test_psychedelia(self):
-        from programs.psychedelia import run
+        from app.programs.psychedelia import run
         self.program_helper(run)
 
     def test_rain(self):
-        from programs.rain import run
+        from app.programs.rain import run
         self.program_helper(run)
 
     def test_rainbow(self):
-        from programs.rainbow import run
+        from app.programs.rainbow import run
         self.program_helper(run)
 
     def test_random_blinky(self):
-        from programs.random_blinky import run
+        from app.programs.random_blinky import run
         self.program_helper(run)
 
     def test_random_sparkles(self):
-        from programs.random_sparkles import run
+        from app.programs.random_sparkles import run
         self.program_helper(run)
 
     def test_simple(self):
-        from programs.simple import run
+        from app.programs.simple import run
         self.program_helper(run)
 
     def test_snow(self):
-        from programs.snow import run
+        from app.programs.snow import run
         self.program_helper(run)
 
     def test_trig(self):
-        from programs.trig import run
+        from app.programs.trig import run
         self.program_helper(run)
 
 
