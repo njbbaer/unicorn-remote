6b33cadac0e7a52eed0f2df4776d7e12d9e729dc
Nathaniel Baer <njbbaer@gmail.com>
2017-08-09 08:00:00 -0700

    Add button click handlers & enable start button when program selected

--- app/templates/static/scripts.js
+++ app/templates/static/scripts.js
@@ -1,10 +1,13 @@
 
-$(".dropdown-menu li a").click(function(){
-  $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
-  $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+// Programs dropwdown
+$('.dropdown-menu li a').click(function(){
+  $(this).parents('.dropdown').find('.btn').html($(this).text() + ' <span class="caret"></span>');
+  $(this).parents('.dropdown').find('.btn').val($(this).data('value'));
+  $('#start-button').prop('disabled', false);
 });
 
 
+// Brightness slider
 $('#brightness').slider({
   formatter: function(value) {
     return value;
@@ -12,6 +15,19 @@ $('#brightness').slider({
 });
 
 
+// Rotation radio buttons
 $('.colors input[type=radio]').on('change', function() {
     console.log(this.value);
+});
+
+
+// Click run button
+$('#run-button').click(function(e) {
+    console.log('Pressed run button')
+});
+
+
+// Click clear button
+$('#stop-button').click(function(e) {
+    console.log('pressed stop button')
 });
