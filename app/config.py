4af8e31da47b321cfbd84223619379167c9c7d3b
Nathaniel Baer <njbbaer@gmail.com>
2017-08-09 08:00:00 -0700

    Add config file with list of programs

---
+++ app/config.py
@@ -0,0 +1,62 @@
+programs = {
+    "ascii_text": {
+        "title": "ASCII Text",
+        "path": "programs/ascii_text.py",
+    },
+    "blink_sun": {
+        "title": "Blink Sun",
+        "path": "programs/blink_sun.py"
+    },
+    "cheertree": {
+        "title": "Cheertree",
+        "path": "programs/cheertree.py"
+    },
+    "cross": {
+        "title": "Cross",
+        "path": "programs/cross.py"
+    },
+    "demo": {
+        "title": "Demo",
+        "path": "programs/demo.py"
+    },
+    "dna": {
+        "title": "DNA",
+        "path": "programs/dna.py"
+    },
+    "game_of_life": {
+        "title": "Game of Life",
+        "path": "programs/game_of_life.py"
+    },
+    "matrix": {
+        "title": "Matrix",
+        "path": "programs/matrix.py"
+    },
+    "psychedelia": {
+        "title": "Psychedelia",
+        "path": "programs/psychedelia.py"
+    },
+    "rain": {
+        "title": "Rain",
+        "path": "programs/rain.py"
+    },
+    "random_blinky": {
+        "title": "Random Blinky",
+        "path": "programs/random_blinky.py"
+    },
+    "random_sparkles": {
+        "title": "Random Sparkles",
+        "path": "programs/random_sparkles.py"
+    },
+    "simple": {
+        "title": "Simple",
+        "path": "programs/simple.py"
+    },
+    "snow": {
+        "title": "Snow",
+        "path": "programs/snow.py"
+    },
+    "trig": {
+        "title": "Trig",
+        "path": "programs/trig.py"
+    }
+}
