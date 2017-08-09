1d0eb8b19891b5769f717d2449c2aeb68ff8847d
Nathaniel Baer <njbbaer@gmail.com>
2017-08-08 23:21:21 -0700

    Update API endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,14 @@ import atexit
 
 from app.views import index
 from app.state import state
-from app.api import PostProgram
+from app.api import SetProgram, StopProgram
 
 
 app = Flask(__name__)
 app.register_blueprint(index)
 
 api = Api(app)
-api.add_resource(PostProgram, '/api/<string:program_name>')
+api.add_resource(SetProgram, '/api/program/<string:program_name>')
+api.add_resource(StopProgram, '/api/program')
 
 atexit.register(state.stop_program)
--- app/api.py
+++ app/api.py
@@ -3,13 +3,21 @@ from flask_restful import Resource, abort
 
 from app.state import state
 
+print("IMPORTED")
 
-class PostProgram(Resource):
 
-    def post(self, program_name):
+class SetProgram(Resource):
+
+    def put(self, program_name):
             args = request.args
 
             try:
                 state.start_program(program_name, args)
             except ValueError as e:
                 abort(400, error=str(e))
+
+
+class StopProgram(Resource):
+
+    def delete(self):
+        state.stop_program()
