e071e32cf98600d1e508a42acb43dc954c6b2e97
Nathaniel Baer <njbbaer@gmail.com>
2017-08-09 08:00:00 -0700

    Output data from form when start button is pressed

--- app/templates/static/scripts.js
+++ app/templates/static/scripts.js
@@ -1,33 +1,21 @@
-
-// Programs dropwdown
 $('.dropdown-menu li a').click(function(){
   $(this).parents('.dropdown').find('.btn').html($(this).text() + ' <span class="caret"></span>');
   $(this).parents('.dropdown').find('.btn').val($(this).data('value'));
   $('#start-button').prop('disabled', false);
 });
 
-
-// Brightness slider
-$('#brightness').slider({
+$('#brightness-slider').slider({
   formatter: function(value) {
     return value;
   }
 });
 
-
-// Rotation radio buttons
-$('.colors input[type=radio]').on('change', function() {
-    console.log(this.value);
+$('#start-button').click(function(e) {
+    console.log($('#programs-dropdown').val())
+    console.log($('#brightness-slider').val())
+    console.log($('input[name=rotation]:checked').val())
 });
 
-
-// Click run button
-$('#run-button').click(function(e) {
-    console.log('Pressed run button')
-});
-
-
-// Click clear button
 $('#stop-button').click(function(e) {
     console.log('pressed stop button')
 });
