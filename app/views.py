d229f76430f542a30ac8476776562f90fcb59aa8
Nathaniel Baer <njbbaer@gmail.com>
2017-08-12 01:37:07 -0700

    Add full programs list to web ui

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from flask import render_template, request, Blueprint
 import json
 
 from app.state import state
+from app.config import programs
 
 
 index = Blueprint('index', __name__, template_folder='templates')
@@ -9,4 +10,4 @@ index = Blueprint('index', __name__, template_folder='templates')
 def show():
     
     if request.method == 'GET':
-         return render_template('index.html')
+         return render_template('index.html', programs=programs)
