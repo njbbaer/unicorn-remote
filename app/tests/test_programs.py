fea54797975017ba85a6b533a71a5600c785d220
Nathaniel Baer <njbbaer@gmail.com>
2017-08-13 23:23:39 -0700

    Refactor test_programs

--- app/tests/test_programs.py
+++ app/tests/test_programs.py
@@ -2,78 +2,60 @@ import unittest
 import multiprocessing
 import time
 
+import app.programs
 
 class TestPrograms(unittest.TestCase):
 
     def program_helper(self, func, params=dict()):
         self.process = multiprocessing.Process(target=func, args=(params,))
         self.process.start()
-        time.sleep(1)
+        time.sleep(0.2)
         self.process.terminate()
 
-    def test_program_ascii_text(self):
-        from app.programs.ascii_text import run
-        self.program_helper(run, { "text": "Hello World" }) 
+    def test_ascii_text(self):
+        self.program_helper(app.programs.ascii_text.run, { "text": "Hello World" }) 
 
     def test_cheertree(self):
-        from app.programs.cheertree import run
-        self.program_helper(run)
+        self.program_helper(app.programs.cheertree.run)
 
     def test_cross(self):
-        from app.programs.cross import run
-        self.program_helper(run)
+        self.program_helper(app.programs.cross.run)
 
     def test_demo(self):
-        from app.programs.demo import run
-        self.program_helper(run)
+        self.program_helper(app.programs.demo.run)
 
     def test_dna(self):
-        from app.programs.dna import run
-        self.program_helper(run)
+        self.program_helper(app.programs.dna.run)
 
     def test_game_of_life(self):
-        from app.programs.game_of_life import run
-        self.program_helper(run)
+        self.program_helper(app.programs.game_of_life.run)
 
-    def test_dna(self):
-        from app.programs.dna import run
-        self.program_helper(run)
-
-    def test_program_matrix(self):
-        from app.programs.matrix import run
-        self.program_helper(run)
+    def test_matrix(self):
+        self.program_helper(app.programs.matrix.run)
 
     def test_psychedelia(self):
-        from app.programs.psychedelia import run
-        self.program_helper(run)
+        self.program_helper(app.programs.psychedelia.run)
 
     def test_rain(self):
-        from app.programs.rain import run
-        self.program_helper(run)
+        self.program_helper(app.programs.rain.run)
 
     def test_rainbow(self):
-        from app.programs.rainbow import run
-        self.program_helper(run)
+        self.program_helper(app.programs.rainbow.run)
 
     def test_random_blinky(self):
-        from app.programs.random_blinky import run
-        self.program_helper(run)
+        self.program_helper(app.programs.random_blinky.run)
 
     def test_random_sparkles(self):
-        from app.programs.random_sparkles import run
-        self.program_helper(run)
+        self.program_helper(app.programs.random_sparkles.run)
 
     def test_simple(self):
-        from app.programs.simple import run
-        self.program_helper(run)
+        self.program_helper(app.programs.simple.run)
 
     def test_snow(self):
-        from app.programs.snow import run
-        self.program_helper(run)
+        self.program_helper(app.programs.snow.run)
 
     def test_trig(self):
-        from app.programs.trig import run
-        self.program_helper(run)
+        self.program_helper(app.programs.trig.run)
 
 
 if __name__ == '__main__':
