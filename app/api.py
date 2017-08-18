af00583052e9c0433418241f79eeeaa853d02efe
Nathaniel Baer <njbbaer@gmail.com>
2017-08-17 21:17:44 -0700

    Fix indentation

--- app/api.py
+++ app/api.py
@@ -8,18 +8,18 @@ from app.state import ProgramNotFound
 class SetProgram(Resource):
 
     def put(self, program):
-            parser = reqparse.RequestParser()
-            parser.add_argument('brightness', type=float)
-            parser.add_argument('rotation', type=int)
-            args = parser.parse_args()
+        parser = reqparse.RequestParser()
+        parser.add_argument('brightness', type=float)
+        parser.add_argument('rotation', type=int)
+        args = parser.parse_args()
 
-            try:
-                state.start_program(program, args)
-            except ProgramNotFound as e:
-                abort(404, error="Program '{}' not found".format(e))
-            except ValueError as e:
-                abort(400, error=str(e))
-            return {"success": "Program '{}' started".format(program)}
+        try:
+            state.start_program(program, args)
+        except ProgramNotFound as e:
+            abort(404, error="Program '{}' not found".format(e))
+        except ValueError as e:
+            abort(400, error=str(e))
+        return {"success": "Program '{}' started".format(program)}
 
 
 class StopProgram(Resource):
