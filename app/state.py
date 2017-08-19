600aa85c7948d68ce2f38e80a9f2d90f6ef69651
Nathaniel Baer <njbbaer@gmail.com>
2017-08-18 22:59:42 -0700

    Allow state to set unicorn hat model

--- app/state.py
+++ app/state.py
@@ -4,28 +4,39 @@ import importlib
 import sys
 import os
 
+import unicornhat
+import unicornhathd
+import app.programs.original
 import app.programs.hd
 
 
 class State:
     ''' Handles the Unicorn HAT state'''
 
-    def __init__(self):
+    def __init__(self, is_hd=True):
         self._process = None
+
+        if is_hd is True:
+            self._unicornhat = unicornhathd
+            self._app_programs = app.programs.hd
+        else:
+            self._unicornhat = unicornhat
+            self. app_programs = app.programs.original
+
 
     def start_program(self, name, params={}):
         try:
-            program = getattr(app.programs.hd, name)
+            program = getattr(self._app_programs, name)
         except AttributeError:
             raise ProgramNotFound(name)
 
         self.stop_program()
 
         if params.get("brightness") is not None:
-            unicornhat.brightness(float(params["brightness"]))
+            self._unicornhat.brightness(float(params["brightness"]))
 
         if params.get("rotation") is not None:
-            unicornhat.rotation(int(params["rotation"]))
+            self._unicornhat.rotation(int(params["rotation"]))
 
         self._process = multiprocessing.Process(target=program.run, args=(params,))
         self._process.start()
@@ -33,7 +44,7 @@ class State:
     def stop_program(self):
         if self._process is not None:
             self._process.terminate()
-        unicornhat.show()
+        self._unicornhat.show()
 
 
 class ProgramNotFound(Exception):
