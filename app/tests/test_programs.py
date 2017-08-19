0aedcc8e3fad660a4d9ac25095a9ae612eba661c
Nathaniel Baer <njbbaer@gmail.com>
2017-08-19 01:47:14 -0700

    Add program tests for hd programs

--- app/tests/test_programs.py
+++ app/tests/test_programs.py
@@ -2,61 +2,33 @@ import unittest
 import multiprocessing
 import time
 
+import app.programs.hd
 import app.programs.original
+
+
+HD_PROGRAMS_LIST = ["candle", "demo", "forest_fire", "game_of_life", "rainbow", 
+    "stars", "trig"]
+ORIGINAL_PROGRAMS_LIST = ["ascii_text", "cheertree", "cross", "demo", "dna", 
+    "game_of_life", "matrix", "psychedelia", "rain", "rainbow", 
+    "random_blinky", "random_sparkles", "simple", "snow", "trig"]
+
 
 class TestPrograms(unittest.TestCase):
 
-    def program_helper(self, func, params=dict()):
+    def run_program(self, func, params=dict()):
         self.process = multiprocessing.Process(target=func, args=(params,))
         self.process.start()
         time.sleep(0.2)
         self.process.terminate()
 
-    def test_ascii_text(self):
-        self.program_helper(app.programs.original.ascii_text.run, { "text": "Hello World" }) 
+    def test_all_hd(self):
+        for program in HD_PROGRAMS_LIST:
+            with self.subTest(program=program):
+                run = getattr(app.programs.hd, program).run
+                self.run_program(run)
 
-    def test_cheertree(self):
-        self.program_helper(app.programs.original.cheertree.run)
-
-    def test_cross(self):
-        self.program_helper(app.programs.original.cross.run)
-
-    def test_demo(self):
-        self.program_helper(app.programs.original.demo.run)
-
-    def test_dna(self):
-        self.program_helper(app.programs.original.dna.run)
-
-    def test_game_of_life(self):
-        self.program_helper(app.programs.original.game_of_life.run)
-
-    def test_matrix(self):
-        self.program_helper(app.programs.original.matrix.run)
-
-    def test_psychedelia(self):
-        self.program_helper(app.programs.original.psychedelia.run)
-
-    def test_rain(self):
-        self.program_helper(app.programs.original.rain.run)
-
-    def test_rainbow(self):
-        self.program_helper(app.programs.original.rainbow.run)
-
-    def test_random_blinky(self):
-        self.program_helper(app.programs.original.random_blinky.run)
-
-    def test_random_sparkles(self):
-        self.program_helper(app.programs.original.random_sparkles.run)
-
-    def test_simple(self):
-        self.program_helper(app.programs.original.simple.run)
-
-    def test_snow(self):
-        self.program_helper(app.programs.original.snow.run)
-
-    def test_trig(self):
-        self.program_helper(app.programs.original.trig.run)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_all_original(self):
+        for program in ORIGINAL_PROGRAMS_LIST:
+            with self.subTest(program=program):
+                run = getattr(app.programs.original, program).run
+                self.run_program(run)
